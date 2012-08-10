cc865d43859e9fc090bc153fe1ee442ca2b0958c
ryanhellyer <ryanhellyer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-10 21:58:13 +0000

    removed need for jQuery AGAIN and added moved enqueue to form field area to stop it serving JS errors in console and added JS headers to dynamic JS file
    git-svn-id: https://plugins.svn.wordpress.org/spam-destroyer/tags/1.2.4@584067 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kill.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Preparing to launch the spam attack
+ * Deploys cookie and sets hidden input field via dynamically generated javascript file
+ *
+ * This file should never be cached
+ * 
+ * @since 1.0
+ * @author Ryan Hellyer <ryan@pixopoint.com>
+ */
+
+
+/*
+ * Quick security check to make sure they aren't up to mischief
+ *
+ * @since 1.2.1
+ * @author Ryan Hellyer <ryan@pixopoint.com>
+ */
+$key = $_GET['key'];
+if ( preg_match( '/[^a-z_\-0-9]/i', $key ) ) {
+	die( "Errr, that wasn't a valid key." );
+}
+
+
+/*
+ * Set appropriate mime-type
+ *
+ * @since 1.2.4
+ * @author Ryan Hellyer <ryan@pixopoint.com>
+ */
+header( 'Content-Type: application/javascript' );
+
+
+/*
+ * Deploy cookie payload
+ *
+ * @since 1.0
+ * @author Ryan Hellyer <ryan@pixopoint.com>
+ */
+if ( ! isset( $_COOKIE[ $key ] ) ) {
+	@setcookie(
+		$key,   // The payload :)
+		time(),         // Cookie creation time
+		time() + ( 60 * 10 ), // Expiry time, set to 10 mins to prevent the nonce expiry (used to generate the key) from interfering
+		'/'             // Available across whole domain
+	);
+}
+
+
+/*
+ * Execute hidden input field payload
+ *
+ * @since 1.2.4
+ * @author Bjørn Johansen <https://twitter.com/bjornjohansen>
+ */
+echo "try {
+	document.getElementById('killer_value').value = '$key';
+} catch (e) {}";
